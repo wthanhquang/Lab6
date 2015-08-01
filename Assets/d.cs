686f36fec1dadf07742939fc0d6639efab1778ba
wthanhquang <w.thanhquang@gmail.com>
2015-08-01 13:52:24 +0700

    Lab6
    Mario

---
+++ Assets/d.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class d : MonoBehaviour {
+	
+	public float timer;
+	public string levelToLoad = "Main";
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("DisplayScene");
+	}
+	
+	IEnumerator DisplayScene(){
+		yield return new WaitForSeconds( timer );
+		Application.LoadLevel (levelToLoad);
+	}
+}
